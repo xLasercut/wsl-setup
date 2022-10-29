4f788f415207682d8d182f44f28434f69c15c5d1
xLasercut <tianxun.wang2004@gmail.com>
2022-10-29 15:31:10 +0100

    updated wsl setup

--- setup.sh
+++ setup.sh
@@ -5,15 +5,10 @@ export ADD_EXECUTABLE=1
 git submodule init
 git submodule update
 
-sudo zypper ref
-sudo zypper dup --no-recommends
-
 pushd ./linux-home-config
 ./setup.sh
 popd
 
-echo "LS_COLORS=\$LS_COLORS:'ow=1;34:' ; export LS_COLORS" >> ~/.zshrc
-
-sudo zypper install --no-recommends zsh youtube-dl
+sudo sudo apt-get install zsh
 
 chsh -s /bin/zsh
