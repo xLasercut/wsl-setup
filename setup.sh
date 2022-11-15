6bf50043d656a7e79c8eb1f792535949a4a231ad
Tim <tianxun.wang1@nhs.net>
2022-11-15 11:13:16 +0000

    updated setup.sh and submodules

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-export ADD_EXECUTABLE=1
-
 git submodule init
 git submodule update
+git submodule update --recursive --remote
+
+# update system
+sudo apt-get update
+sudo sudo apt-get install zsh
 
 pushd ./linux-home-config
-./setup.sh
+source linux_setup.sh
+linux_setup setup base
+linux_setup executables
 popd
 
 sudo sudo apt-get install zsh
