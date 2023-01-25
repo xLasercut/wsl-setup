6fb26bc7711bd891de08563911e694c2c0450266
Tim Wang <tianxun.wang1@nhs.net>
2023-01-25 15:12:48 +0000

    added x11 forwarding support

--- setup.sh
+++ setup.sh
@@ -6,10 +6,16 @@ git submodule update --recursive --remote
 
 # update system
 sudo apt-get update
-sudo sudo apt-get install zsh
+sudo sudo apt-get install zsh libxss1 libfuse2 software-properties-common unzip
+sudo add-apt-repository ppa:appimagelauncher-team/stable
+sudo apt-get update
+sudo apt-get install appimagelauncher
 
 pushd ./linux-home-config
 source linux_setup.sh
 linux_setup setup base
 linux_setup executables
 popd
+
+echo $'export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk \'{print $2; exit;}\'):0.0' >> ~/.zshrc
+echo 'export PATH="$HOME/.local/share/JetBrains/Toolbox/scripts:$PATH"' >> ~/.zshrc
