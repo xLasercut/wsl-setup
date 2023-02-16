85b10f6d6ddaea099e63bf4970d124a47132582c
Tim Wang <tianxun.wang1@nhs.net>
2023-02-16 16:28:10 +0000

    removed wslg setup

--- setup.sh
+++ setup.sh
@@ -6,17 +6,10 @@ git submodule update --recursive --remote
 
 # update system
 sudo apt-get update
-sudo sudo apt-get install zsh libxss1 libfuse2 software-properties-common unzip libasound2 libsecret-1-0 gnome-keyring libcups2 libpangocairo-1.0-0 libatk-adaptor libnss3 libxcb-keysyms1 x11-apps libgbm1 libxkbcommon-dev
-sudo add-apt-repository ppa:appimagelauncher-team/stable
-sudo apt-get update
-sudo apt-get install appimagelauncher
+sudo sudo apt-get install zsh
 
 pushd ./linux-home-config
 source linux_setup.sh
 linux_setup setup base
-linux_setup executables
+linux_setup aws
 popd
-
-echo $'export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk \'{print $2; exit;}\'):0.0' >> ~/.zshrc
-echo 'export LIBGL_ALWAYS_INDIRECT=0' >> ~/.zshrc
-echo 'export PATH="$HOME/.local/share/JetBrains/Toolbox/scripts:$HOME/.local/share/JetBrains/Toolbox/bin:$PATH"' >> ~/.zshrc
