0378defb50f35276c1dc5c2743d341127f96fa8a
Tim Wang <tianxun.wang1@nhs.net>
2023-07-19 10:41:48 +0100

    removed submodule

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,3 +11,8 @@ sudo apt update
 sudo apt install zsh build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev git
 
 git clone https://github.com/zsh-users/antigen.git ~/antigen
+
+cp ./roles/zsh/files/.zshrc ~
+cp ./roles/zsh/files/.p10k.zsh ~
+
+chsh -s /bin/zsh
