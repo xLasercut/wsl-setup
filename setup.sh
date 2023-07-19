56e9f85ac7330b2a00c237739897b2cf1a909b52
Tim Wang <tianxun.wang1@nhs.net>
2023-07-19 10:43:50 +0100

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-git submodule init
-git submodule update
-git submodule update --recursive --remote
-
-# update system
-sudo apt-get update
-sudo sudo apt-get install zsh
-
-pushd ./linux-home-config
-source linux_setup.sh
-linux_setup setup base
-linux_setup executables
-popd
+ansible-playbook wsl-setup.yml -Kvk
