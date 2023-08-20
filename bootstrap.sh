7228b540b59d9175e5a370c7a5d645ac32ad11d8
xLasercut <tianxun.wang2004@gmail.com>
2023-08-20 09:10:08 +0100

    moved to wsl1

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,13 +9,13 @@ fi
 
 sudo zypper ref
 sudo zypper install zsh git gcc automake bzip2 libbz2-devel xz xz-devel openssl-devel ncurses-devel readline-devel zlib-devel tk-devel libffi-devel sqlite3-devel gdbm-devel make findutils patch
-sudo zypper install -t pattern wsl_base wsl_systemd wsl_gui
+sudo zypper install -t pattern wsl_base
 
 git clone https://github.com/zsh-users/antigen.git ~/antigen
 
 curl "https://raw.githubusercontent.com/xLasercut/my-zsh/master/.zshrc" --output "${HOME}/.zshrc"
 curl "https://raw.githubusercontent.com/xLasercut/my-zsh/master/.p10k.zsh" --output "${HOME}/.p10k.zsh"
-sudo cp ./roles/wsl_config/files/wsl.conf /etc
+# sudo cp ./roles/wsl_config/files/wsl.conf /etc
 
 chsh -s /bin/zsh
 
